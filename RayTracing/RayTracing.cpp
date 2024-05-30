3556057b6ae96206e2d151a5a98db75c06e71e1f
Iwan28TT <iwandejager000@gmail.com>
2024-05-30 12:10:36 +0200

    V1
    Phong

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -1,20 +1,126 @@
-// RayTracing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// Created by Bardio on 22/05/2024.
 //
 
-#include <bardrix/vector3.h>
 #include <iostream>
-int main()
-{
-    std::cout << bardrix::vector3(1, 2, 3) << std::endl;
+
+#ifdef _WIN32
+
+#include "sphere.h"
+#include "window.h"
+
+#include <bardrix/ray.h>
+#include <bardrix/light.h>
+#include <bardrix/camera.h>
+#include <bardrix/quaternion.h> // Ensure this header is included for bardrix::quaternion
+
+
+/// \brief Calculates the light intensity at a given intersection point
+/// \param shape The shape that was intersected
+/// \param light The light source
+/// \param camera The camera
+/// \param intersection_point The intersection point of an object
+/// \return The light intensity at the intersection point
+/// \example double intensity = calculate_light_intensity(shape, light, camera, intersection_point);
+double calculate_light_intensity(const bardrix::shape& shape, const bardrix::light& light, const bardrix::camera& camera,
+const bardrix::point3& intersection_point) {
+const bardrix::vector3 light_intersection_vector = intersection_point.vector_to(light.position).normalized();
+
+// Angle between the normal and the light intersection vector
+const double angle = shape.normal_at(intersection_point).dot(light_intersection_vector);
+
+if (angle < 0) // This means the light is behind the intersection_point
+return 0;
+
+// Specular reflection
+bardrix::vector3 reflection = bardrix::quaternion::mirror(light_intersection_vector,
+shape.normal_at(intersection_point));
+double specular_angle = reflection.dot(camera.position.vector_to(intersection_point).normalized());
+double specular = std::pow(specular_angle, shape.get_material().get_shininess());
+
+// We're calculating phong shading (ambient + diffuse + specular)
+double intensity = shape.get_material().get_ambient();
+intensity += shape.get_material().get_diffuse() * angle;
+intensity += shape.get_material().get_specular() * specular;
+
+// Max intensity is 1
+return min(1.0, intensity * light.inverse_square_law(intersection_point));
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    int width = 600;
+    int height = 600;
+    // Create a window
+    bardrix::window window("Raytracing", width, height);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Create a camera
+    bardrix::camera camera = bardrix::camera({ 0,0,0 }, { 0,0,1 }, width, height, 60);
+
+    // Create a sphere
+    sphere sphere(1.0, bardrix::point3(0.0, 0.0, 3.0), bardrix::material(0.1, 1, 0.5, 50));
+
+    //Create a light
+    bardrix::light light = bardrix::light({ -1,0,-1 }, 4, bardrix::color::cyan());
+    bardrix::light light2 = bardrix::light({ 1,0,1 }, 1, bardrix::color::cyan());
+
+    // [&camera, &sphere] is a capture list, this means we can access those objects outside the lambda
+    // If you'd want to add a light you'd have to add this to the capture list too.
+    window.on_paint = [&camera, &sphere, &light, &light2](bardrix::window* window, std::vector<uint32_t>& buffer) {
+        // Go through all the pixels
+        for (int y = 0; y < window->get_height(); y++) {
+            for (int x = 0; x < window->get_width(); x++) {
+
+                // Shoot a ray from the camera to the pixel
+                bardrix::ray ray = *camera.shoot_ray(x, y, 10);
+
+                // Optional means that we can or cannot have a value
+                // In order to check if the optional has a value, we use the has_value() method
+                // If the optional has a value, we can access it using the value() method
+                // E.g. intersection.value() -> bardrix::point3
+                std::optional<bardrix::point3> intersection = sphere.intersection(ray);
+
+                // Default color is black
+                bardrix::color color = bardrix::color::black();
+
+                // If the ray intersects the sphere, paint the pixel white
+                if (intersection.has_value()) {
+                    double intensity = calculate_light_intensity(sphere, light, camera, intersection.value());
+                    intensity += calculate_light_intensity(sphere, light2, camera, intersection.value());
+                    color = light.color * intensity;
+                }
+
+                // Set the pixel
+                buffer[y * window->get_width() + x] = color.argb(); // ARGB is the format used by Windows API
+            }
+        }
+        };
+
+    window.on_resize = [&camera](bardrix::window* window, int width, int height) {
+        // Resize the camera
+        camera.set_width(width);
+        camera.set_height(height);
+
+        window->redraw(); // Redraw the window (calls on_paint)
+        };
+
+    // Get width and height of the screen
+    int screen_width = GetSystemMetrics(SM_CXSCREEN);
+    int screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+    // Show the window in the center of the screen
+    if (!window.show(screen_width / 2 - width / 2, screen_height / 2 - height / 2)) {
+        std::cout << GetLastError() << std::endl;
+        return -1;
+    }
+
+    bardrix::window::run();
+}
+
+#else // _WIN32
+
+int main() {
+    std::cout << "This example is only available on Windows." << std::endl;
+    return 0;
+}
+
+#endif // _WIN32
